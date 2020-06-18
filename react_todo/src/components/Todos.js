90df710261e952648b9e10aa0e4af95887a46fd8
Ameya <ameyaingale1@gmail.com>
2020-06-17 20:17:30 -0700

    Test

---
+++ react_todo/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import TodoItem from './TodoItem';
+
+class Todos extends Component {
+  render(){
+
+  return this.props.todo.map((i) => (
+    <TodoItem key={i.id} todoitem = {i}/>
+  ))
+  }
+}
+
+export default Todos;
